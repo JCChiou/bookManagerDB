211e702280f437255e9a7de7513307169088e926
JCCHiou <rex2763@gmail.com>
2021-01-29 16:38:54 +0800

    create a Room base book Manager app

---
+++ app/src/main/java/com/example/bookmanagerdb/database/BookStore.kt
@@ -0,0 +1,17 @@
+package com.example.bookmanagerdb.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "my_bookstore_table")
+data class BookStore (
+    @PrimaryKey(autoGenerate = true)
+    var _id: Int ,
+
+    @ColumnInfo(name = "book_name")
+    var bookName: String,
+
+    @ColumnInfo(name = "book_price")
+    var bookPrice: Int
+    )
